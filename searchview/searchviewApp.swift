77b34474fcb333aaa354deb6e46ac7e818b516d1
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-03-18 19:37:44 -0300

    Initial Commit

---
+++ searchview/searchviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  searchviewApp.swift
+//  searchview
+//
+//  Created by Jonn Alves on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct searchviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
